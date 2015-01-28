af4005f8e070796afb9ab7e62f31226d5ba25f5e
tomykaira <tomykaira@gmail.com>
2015-01-28 16:17:40 +0900

    Show example to use fake_event for receive_webhook

--- spec/fake_entity_spec.rb
+++ spec/fake_entity_spec.rb
@@ -1,4 +1,6 @@
 require 'spec_helper'
+require 'webpay'
+
 describe WebPay::Mock::FakeEntity do
   include WebPay::Mock::FakeEntity
 
@@ -8,5 +10,15 @@ describe WebPay::Mock::FakeEntity do
     specify { expect(list['url']).to eq '/v1/numbers' }
     specify { expect(list['count']).to eq 3 }
     specify { expect(list['data']).to eq [1, 2, 3] }
+  end
+
+  describe 'fake webhook request from event' do
+    let(:webpay) { WebPay.new('test_secret_xxxxxx') }
+
+    it 'should accept string event as webhook body' do
+      fake = fake_event
+      received = webpay.receive_webhook(fake.to_json)
+      expect(received.id).to eq fake['id']
+    end
   end
 end
